da3f3bd74b0e04a7636a1e35286281db01aec5b1
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-09-17 18:28:57 -0300

    Copying Domain layer

---
+++ src/domain/forum/application/use-cases/get-question-by-slug.ts
@@ -0,0 +1,32 @@
+import { Question } from '@/domain/forum/enterprise/entities/question';
+import { QuestionsRepository } from '../repositories/questions-repository';
+import { Either, right } from '@/core/either';
+
+interface GetQuestionBySlugUseCaseRequest {
+  slug: string;
+}
+
+type GetQuestionBySlugUseCaseResponse = Either<
+  null,
+  {
+    question: Question;
+  }
+>;
+
+export class GetQuestionBySlugUseCase {
+  constructor(private questionsRepository: QuestionsRepository) {}
+
+  async execute({
+    slug,
+  }: GetQuestionBySlugUseCaseRequest): Promise<GetQuestionBySlugUseCaseResponse> {
+    const question = await this.questionsRepository.findBySlug(slug);
+
+    if (!question) {
+      throw new Error('Question not found.');
+    }
+
+    return right({
+      question,
+    });
+  }
+}
