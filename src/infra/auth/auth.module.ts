6d129f4759c1f87eb601088e8ee9f8de020e9686
Dener Araujo (Natsume) <natsume2b.me@gmail.com>
2023-09-19 11:13:31 -0300

    Private routes by default

--- src/infra/auth/auth.module.ts
+++ src/infra/auth/auth.module.ts
@@ -4,6 +4,8 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
 import { Env } from '../env';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -23,6 +25,12 @@ import { Env } from '../env';
       },
     }),
   ],
-  providers: [JwtStrategy],
+  providers: [
+    JwtStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: JwtAuthGuard,
+    },
+  ],
 })
 export class AuthModule {}
